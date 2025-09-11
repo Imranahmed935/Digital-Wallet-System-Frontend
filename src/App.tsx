087d3729fcd45f886c4d3ea130f27a2e3bb99fd6
Imranahmed935 <imrantahir9918@gmail.com>
2025-09-11 15:05:34 +0600

    Project setup Completed

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+
+
+  return (
+   <div>
+    <h1>hello i am ready to go</h1>
+    <Button>Done</Button>
+   </div>
+  )
+}
+
+export default App
