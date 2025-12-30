7f084ff0200fd5f3d30c1909bc28c841ccf88d16
Imranahmed935 <imrantahir9918@gmail.com>
2025-12-30 23:22:59 +0600

    vercel deploy issue solve

--- src/components/Layouts/CommonLayout.tsx
+++ src/components/Layouts/CommonLayout.tsx
@@ -1,33 +1,15 @@
 import type { ReactNode } from "react";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
-import GuidedTour from "../GuidedTour";
-import { useUserInfoQuery } from "@/redux/features/auth/auth.api";
-import { useAllUsersQuery, useAllAgentsQuery, useAllTransactionsQuery } from "@/redux/features/admin/admin.api";
 
 interface IProps {
   children: ReactNode;
 }
 
 const CommonLayout = ({ children }: IProps) => {
-  const { data: userData } = useUserInfoQuery(undefined);
-  const { isLoading: usersLoading } = useAllUsersQuery(undefined);
-  const { isLoading: agentsLoading } = useAllAgentsQuery(undefined);
-  const { isLoading: transactionsLoading } = useAllTransactionsQuery(undefined);
-
-  const userEmail = userData?.data?.email;
-
   return (
     <div className="min-h-screen flex flex-col">
       <Navbar />
-      {userEmail && (
-        <GuidedTour
-          userEmail={userEmail}
-          usersLoaded={!usersLoading}
-          agentsLoaded={!agentsLoading}
-          transactionsLoaded={!transactionsLoading}
-        />
-      )}
       <div className="grow">{children}</div>
       <Footer />
     </div>
